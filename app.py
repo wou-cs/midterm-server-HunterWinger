d378849823e1bcd4f1eb5f571dde4a924d2f6513
HunterWinger <WingerHunter01@gmail.com>
2025-05-04 19:50:04 -0700

    Implemented decrementation and hex conversion for positive integer values

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 from flask import Flask, request
 
-
 app = Flask(__name__)
 
+@app.route("/api/calcs/<value>")
+def calculate(value):
+    # Ensure that our number isn't negative 
+    try:
+        number = int(value)
+        if number <= 0:
+            raise ValueError
+    except ValueError:
+        return "Invalid input", 400
 
-@app.route("/some_route_here", methods=["GET"])
-def method_name():
-    return {}
+    # Construct our JSON string
+    response = (
+        '{'
+        # Number - 1 and format into hex using string interp
+        f'"dec": {number - 1}, '
+        f'"hex": "0x{number:x}"'
+        '}'
+    )
+
+    return response, 200, {"Content-Type": "application/json"}
+
+if __name__ == "__main__":
+    app.run(port=8080)
